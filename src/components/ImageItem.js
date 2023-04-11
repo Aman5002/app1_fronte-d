e9c137a556279666a5f70dc5dff21bf7e1800686
Aman5002 <amanswami5002@gmail.com>
2023-04-11 13:10:25 +0530

    frontend changes

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,15 +1,31 @@
 import React from 'react'
 function ImageItem(props) {
+  const delethandler = async()=>{
+    const response = await fetch('http://localhost:5000/api/images/deleteimage' , {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      },
+      body: JSON.stringify({id:props.img._id})
+  }
+  );
+  // const json = await response.json()
+  props.update()
+  }
   return (
-    <div className="col-md-3">
+    <div className="col-md-4">
     <div className="card my-3">
-    <div className="card" >
-  <img src={props.img.imgUrl ? props.img.imgUrl : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg" } className="card-img-top" style={{height:"300px"}} alt="..."/>
-  <div className="card-body">
+    <div className="card">
+  <a href= {props.img.imgUrl}target='blank'>
+    <img src={props.img.imgUrl ? props.img.imgUrl : "https://letusstudy.in/clientside/images/no-image.png" } className="card-img-top" style={{height:"300px"}} alt="..."/></a>
+  <div className="card-body" >
     <h5 className="card-title">{props.img.title}</h5>
     <h6 className="card-title">#{props.img.tag}</h6>
     <p className="card-text">{props.img.description}</p>
-  </div>
+    <i className=" far fa-edit mx-2"  style={{cursor:"pointer"}}onClick={()=>{window.scrollTo({top: 0, left: 0, behavior: 'smooth'});    props.id(props.img._id); props.edit(true) ; props.title(props.img.title); props.desc(props.img.description); props.tag(props.img.tag); props.url(props.img.imgUrl)   } }></i> 
+    {/* <i class=" far fa-edit"></i> */}
+    <i className="far fa-trash-alt mx-2" style={{cursor:"pointer"}} onClick={delethandler}></i>  </div>
 </div>
     </div>
 </div>
--- src/components/Images.js
+++ src/components/Images.js
@@ -9,6 +9,8 @@ const [title, settitle] = useState("No title specified");
 const [desc, setdesc] = useState("No description specified");
 const [tag, settag] = useState("");
 const [url, seturl] = useState("");
+const [id, setid] = useState("");
+const [edit, setedit] = useState(false);
 useEffect(() => {
   getimages();
  }, []);
@@ -41,6 +43,28 @@ getimages();
 setshowaddbtn(true)
 setshowadditem(false)
 };
+function clearhandler(){
+  settitle("No Title Specified");
+  setdesc("No Description Specified");
+  settag("General");
+  seturl("");
+}
+const handleSubmitedit = async(event) => {
+  event.preventDefault();
+const response = await fetch("http://localhost:5000/api/images/editimage" , {
+method:"PUT",
+headers:{
+  'Content-Type': 'application/json'  ,
+  'auth-token': localStorage.getItem('token')
+},
+  body: JSON.stringify({id : id , title: title , description: desc , tag:tag, imgUrl: url})
+},
+);
+const json = await response.json()
+console.log(json);
+getimages();
+setedit(false)
+};
 const titlechange = (event) => {
  settitle(event.target.value);
 };
@@ -56,13 +80,13 @@ const imgurlchange = (event) => {
   return (
     <>
  {showaddbtn && <div className="container">
-    <button className='btn btn-primary' onClick={()=>{setshowadditem(true); setshowaddbtn(false)}}>Add Item</button>
+    <button className='btn btn-primary' style={edit ? {display:"None"} : {display:"block"}} onClick={()=>{setshowadditem(true); setshowaddbtn(false); clearhandler()}}>Add Item</button>
   </div>}
  { showadditem && <form onSubmit={handleSubmit}>
   <div className="mb-3">
     <h3>Add Item Here</h3>
     <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
-    <input type="text" className="form-control" onChange={titlechange} id="title" aria-describedby="title"/>
+    <input type="text" className="form-control" onChange={titlechange} id="title" aria-describedby="title" required/>
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Description</label>
@@ -79,13 +103,34 @@ const imgurlchange = (event) => {
   <button type="submit" className="btn btn-primary">Submit</button>
   <button  className="btn btn-danger mx-3" onClick={()=>{setshowadditem(false); setshowaddbtn(true)}}>Close</button>
 </form>}
+{edit && <form onSubmit={handleSubmitedit}>
+  <div className="mb-3">
+    <h3>Edit Item Here</h3>
+    <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
+    <input type="text" className="form-control"  value={title} onChange={titlechange} id="title" aria-describedby="title"/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Description</label>
+    <input type="text" className="form-control" value={desc} onChange={deschange} id="desc" aria-describedby="desc"/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Tag</label>
+    <input type="text" className="form-control" value={tag}  onChange={tagchange} id="tag" aria-describedby="tag"/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Image Url</label>
+    <input type="text" className="form-control" value={url} onChange={imgurlchange} id="tag" aria-describedby="imgurl"/>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+  <button  className="btn btn-danger mx-3" onClick={()=>{setedit(false); setshowaddbtn(true)}}>Close</button>
+</form>}
 <div className="row my-3">
                 <h2>Your Images</h2>
                 <div className="container mx-2"> 
                 {images.length===0 && 'No Image to display'}
                 </div>
                 {images.map((img) => {
-                  return <ImageItem key={img._id}  img={img} />
+                  return <ImageItem key={img._id}  img={img} id={setid} edit={setedit} title={settitle} desc={setdesc} tag={settag} url={seturl} update={getimages}/>
                 })}
             </div>
                 </>
